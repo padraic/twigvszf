b6d968264ceed9b18a9762fc2b93bac124136836
Pádraic Brady <padraic.brady@yahoo.com>
2009-10-09 20:32:06 +0100

    Updated to CLI runner and added result output

--- twig.php
+++ twig.php
@@ -1,35 +1,43 @@
 <?php
 
-// edit for own path
 require_once '/home/padraic/projects/misc/twig/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem(
-    dirname(__FILE__) . '/twig_templates',
-    dirname(__FILE__) . '/twig_templates/cache' // must be writable!
-);
-$twig = new Twig_Environment($loader);
-$escaper = new Twig_Extension_Escaper(true);
-$twig->addExtension($escaper);
+$timeStart = microtime(true);
 
-$template = $twig->loadTemplate('index.phtml');
+for ($i = 0; $i < 10000; $i++) {
+    $loader = new Twig_Loader_Filesystem(
+        dirname(__FILE__) . '/twig_templates',
+        dirname(__FILE__) . '/twig_templates/cache'
+    );
+    $twig = new Twig_Environment($loader);
+    $escaper = new Twig_Extension_Escaper(true);
+    $twig->addExtension($escaper);
 
-$vars = array(
-    'navigation' => array(
-        array(
-            'href' => '/foo',
-            'caption' => 'Foo'
-        ),
-        array(
-            'href' => '/bar',
-            'caption' => 'Bar'
-        ),
-        array(
-            'href' => '/blog',
-            'caption' => 'Blog&'
+    $template = $twig->loadTemplate('index.phtml');
+
+    $vars = array(
+        'navigation' => array(
+            array(
+                'href' => '/foo',
+                'caption' => 'Foo'
+            ),
+            array(
+                'href' => '/bar',
+                'caption' => 'Bar'
+            ),
+            array(
+                'href' => '/blog',
+                'caption' => 'Blog&'
+            )
         )
-    ),
-    'foo' => 'Padraic Brady'
-);
+    );
 
-echo $template->render($vars);
+    $template->render($vars);
+}
+
+$timeEnd = microtime(true);
+$timeTotal = $timeEnd - $timeStart;
+echo 'Total Time (s): ', $timeTotal, "\n",
+     'Memory (kb): ', memory_get_peak_usage()/1024, "\n",
+     'Templates (p/s): ', (10000/$timeTotal), "\n";
--- zf.php
+++ zf.php
@@ -2,31 +2,39 @@
 
 function autoload($class) {
     require str_replace('_', '/', $class) . '.php';
-    return $class;
+    return true;
 }
 spl_autoload_register('autoload');
 
-$template = new Zend_View(
-    array(
-        'scriptPath' => dirname(__FILE__) . '/zf_templates'
-    )
-);
+$timeStart = microtime(true);
 
-$template->navigation = array(
-    array(
-        'href' => '/foo',
-        'caption' => 'Foo'
-    ),
-    array(
-        'href' => '/bar',
-        'caption' => 'Bar'
-    ),
-    array(
-        'href' => '/blog',
-        'caption' => 'Blog&'
-    )
-);
+for ($i = 0; $i < 10000; $i++) {
+    $template = new Zend_View(
+        array(
+            'scriptPath' => dirname(__FILE__) . '/zf_templates'
+        )
+    );
 
-$template->foo = 'Padraic Brady';
+    $template->navigation = array(
+        array(
+            'href' => '/foo',
+            'caption' => 'Foo'
+        ),
+        array(
+            'href' => '/bar',
+            'caption' => 'Bar'
+        ),
+        array(
+            'href' => '/blog',
+            'caption' => 'Blog&'
+        )
+    );
 
-echo $template->render('index.phtml');
+    $template->render('index.phtml');
+}
+
+$timeEnd = microtime(true);
+$timeTotal = $timeEnd - $timeStart;
+echo 'Time (s): ', $timeTotal, "\n",
+     'Memory (kb): ', memory_get_peak_usage()/1024, "\n",
+     'Templates (p/s): ', (10000/$timeTotal), "\n";
